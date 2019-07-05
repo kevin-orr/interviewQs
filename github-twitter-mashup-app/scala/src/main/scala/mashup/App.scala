d6f1e6936263bdc3d9807486dfadc7c96489f9b0
kevin-orr <kevinorr.home@gmail.com>
2019-07-05 22:44:40 +0100

    Added closure to capture timings and logging

--- github-twitter-mashup-app/scala/src/main/scala/mashup/App.scala
+++ github-twitter-mashup-app/scala/src/main/scala/mashup/App.scala
@@ -9,35 +9,54 @@ package mashup
   */
 object App {
 
-  def main(args: Array[String]): Unit = {
+    def main(args: Array[String]): Unit = {
 
-    val banner =
-    """
-===========================================================================================
-      This App will search Github for top 10 'Reactive' projects (by default) then for each
-      project it will then search for a few of the most recent tweets that mention that project.
+        val banner =
+        """
+    ===========================================================================================
+        This App will search Github for top 10 'Reactive' projects (by default) then for each
+        project it will then search for a few of the most recent tweets that mention that project.
 
-      You can configure the App via the configuration file 'properties.config' which the App will
-      look for in same directory that the uber jar lives.
+        You can configure the App via the configuration file 'properties.config' which the App will
+        look for in same directory that the uber jar lives.
 
-      You can configure the following details:
-        # The Twitter consumer and access tokens
-        consumer.token.key=???
-        consumer.token.secret=???
-        access.token.key=???
-        access.token.secret=???
-        # A project name to search GitHub - defaul is 'Reactive'
-        github.project=Reactive
+        You can configure the following details:
+            # The Twitter consumer and access tokens
+            consumer.token.key=???
+            consumer.token.secret=???
+            access.token.key=???
+            access.token.secret=???
+            # A project name to search GitHub - defaul is 'Reactive'
+            github.project=Reactive
 
-      When complete any response will be written to 'twits.json' in same directory.
+        When complete any response will be written to 'twits.json' in same directory.
 
-===========================================================================================
-      """.stripMargin
+    ===========================================================================================
+        """.stripMargin
 
-    println(banner)
+        println(banner)
 
+        withTiming {
+            // no tasks to execute - yet!
+        }
   }
 
+    // We could do more logging of metrics here perhaps
+    def withTiming(body: => Unit): Unit = {
+        // start timing from, wait for it, now...
+        val startTime = System.currentTimeMillis
+        // now execute the task(s)
+        body
+        // and record how long it took
+        val endTime = System.currentTimeMillis
+        // log it
+        println(
+        s"""
+            FINSHED!
+
+            App took ${(endTime - startTime) / 1000} seconds to get results - please check twits-{date-time}.json.
+        """)
+    }
 }
 
 
